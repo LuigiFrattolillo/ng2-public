d7425846eb204b9d337eb0bc0f0ea90243b869e9
Luigi Frattolillo <luifrat@gmail.com>
2016-01-22 10:29:15 +0000

    Create app-injector.ts

---
+++ app/ts/services/app-injector.ts
@@ -0,0 +1,12 @@
+import {Injector} from 'angular2/core';
+let appInjectorRef;
+
+export const appInjector = (injector?: Injector):Injector => {
+	if (!injector) {
+		return appInjectorRef;
+	}
+
+	appInjectorRef = injector;
+
+	return appInjectorRef;
+};
