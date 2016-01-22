4b2df6046f2077a22684c2d5b3dac921bd723501
Luigi Frattolillo <luifrat@gmail.com>
2016-01-22 10:14:34 +0000

    Create vendor.ts

---
+++ vendor.ts
@@ -0,0 +1,10 @@
+// Polyfills
+import 'es6-shim';
+import 'es6-promise';
+//import 'angular2-polyfills';
+import 'es7-reflect-metadata/dist/browser';
+import 'zone.js/lib/browser/zone-microtask';
+import 'zone.js/lib/zones/long-stack-trace';
+import 'jquery/dist/jquery';
+// RxJS
+import 'rxjs';
